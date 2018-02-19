f87b92d686f8fd7c083c2144455d6ab80b03e155
Denys Dorofeiev <dor.denis.de@gmail.com>
2018-02-19 19:45:51 +0100

    Update long description

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 setup(name='jiratracker',
       version='1.0',
       description='Automatically logs development time to Jira based on Git checkout history',
+      long_description='Automatically logs development time to Jira based on Git checkout history. See the documentation at https://github.com/dor-denis/JiraTracker',
       author='Denys Dorofeiev',
       author_email='dor.denis.de@gmail.com',
       url='https://github.com/dor-denis/JiraTracker',
